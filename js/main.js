77414a155ce9eb6e718497eae70cb7508588a13e
chen <164979511@qq.com>
2018-04-07 22:00:52 +0800

    improve nav change suit 20180407 22:00:00

--- js/main.js
+++ js/main.js
@@ -36,8 +36,20 @@ $(function(){
             $('.nav-item.nav-show').removeClass('nav-show');
             $('.nav-item').children('ul').removeAttr('style');
             $('.nav').addClass('nav-mini');
+
+            //点击mini图标，改变左侧导航和右侧的响应式class
+             $('#nav_div_left').removeAttr('class');
+             $('#nav_div_right').removeAttr('class');
+             $('#nav_div_left').addClass('col-xs-1 col-lg-1 col-md-1 col-sm-1');
+             $('#nav_div_right').addClass('col-xs-11  col-lg-11 col-md-11 col-sm11');
         }else{
             $('.nav').removeClass('nav-mini');
+
+            //点击mini图标，改变左侧导航和右侧的响应式class
+             $('#nav_div_left').removeAttr('class');
+             $('#nav_div_right').removeAttr('class');
+             $('#nav_div_left').addClass('col-xs-3 col-lg-3 col-md-3 col-sm-3');
+             $('#nav_div_right').addClass('col-xs-9  col-lg-9 col-md-9 col-sm9');
         }
     });
 
